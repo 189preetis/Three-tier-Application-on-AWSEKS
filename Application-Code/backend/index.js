3796db7b63d8603c3f18c9dd4b7194faf0f61d67
Ubuntu <ubuntu@ip-172-31-15-254.ap-south-1.compute.internal>
2025-05-16 05:35:54 +0000

    update ingress.yml

--- Application-Code/backend/index.js
+++ Application-Code/backend/index.js
@@ -4,16 +4,31 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 
+
+require("dotenv").config();
+
 connection();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/ok', (req, res) => {
-    res.status(200).send('ok')
-  })
+//app.get('/ok', (req, res) => {
+  //  res.status(200).send('ok')
+ // })
+//
+//
+app.get('/', (req, res) => {
+	  res.send('Backend is working!');
+});
 
 app.use("/api/tasks", tasks);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+
+
+// Start server
+// app.listen(port, '0.0.0.0', () => {
+  // console.log(`Server running on port ${port}`);
+// });
